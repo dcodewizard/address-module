42cacd9a53ea5a90e0de0de6d294cf91f2db9e45
Anam Maqbool <am.anammaqbool@gmail.com>
2023-11-01 14:11:24 +0500

    Server: Added Express Async Handler

--- server/routes/address.routes.js
+++ server/routes/address.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler')
 const router = express.Router();
 const AddressCtrl = require('../controllers/address/address.controller');
 
-router.post('/', AddressCtrl.addAddress);
-router.get('/search', AddressCtrl.searchAddress);
-router.get('/', AddressCtrl.getAllAddresses);
-router.get('/:id', AddressCtrl.getAddress);
-router.put('/:id', AddressCtrl.updateAddress);
-router.delete('/:id', AddressCtrl.deleteAddress);
+router.post('/', asyncHandler(AddressCtrl.addAddress));
+router.get('/search', asyncHandler(AddressCtrl.searchAddress));
+router.get('/', asyncHandler(AddressCtrl.getAllAddresses));
+router.get('/:id', asyncHandler(AddressCtrl.getAddress));
+router.put('/:id', asyncHandler(AddressCtrl.updateAddress));
+router.delete('/:id', asyncHandler(AddressCtrl.deleteAddress));
 
 module.exports = router;
